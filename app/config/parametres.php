003432d40abfa86060184febbec11697e0bb2df1
Sylvie <gillissen.sylvie@gmail.com>
2020-05-18 15:20:49 +0200

    Structure MVC initiale
    Mise en place d'une structure MVC intiale

---
+++ app/config/parametres.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+./app/config/parametres.php
+*/
+// PARAMETRES DE CONNEXION A LA DATABASE
+define('HOSTNAME', 'localhost:3306');
+define('DBNAME', 'readit_2020');
+define('USERNAME', 'root');
+define('USERPWD', 'root');
+
+
+
+// initialisation des zones dynamiques du template
+
+$title='';
+$content='';
